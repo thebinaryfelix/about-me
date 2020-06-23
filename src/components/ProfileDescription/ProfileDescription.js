5fb553b0da5837e7f6c08a0411497bb48d00def8
thebinaryfelix <mateus.cfoliveira@gmail.com>
2020-06-23 18:46:21 -0300

    refactor(ProfileDescription): extract Grid

--- src/components/ProfileDescription/ProfileDescription.js
+++ src/components/ProfileDescription/ProfileDescription.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Link, Typography } from '@material-ui/core'
+import { Box, Link, Typography } from '@material-ui/core'
 import React from 'react'
 import LinkedinIcon from '@material-ui/icons/LinkedIn'
 import GithubIcon from '@material-ui/icons/GitHub'
@@ -8,38 +8,33 @@ const ProfileDescription = () => {
   const classes = useStyles()
 
   return (
-    <Grid item container justify="flex-end" className={classes.root} xs={12}>
-      <Grid item xs={12}>
-        <Box
-          width={1}
-          display="flex"
-          alignItems="center"
-          justifyContent="flex-end"
-        >
-          <Box mr={1}>
-            <Link href="https://github.com/thebinaryfelix" target="_blank">
-              <GithubIcon className={classes.githubIcon} />
-            </Link>
-          </Box>
-          <Box>
-            <Link
-              href="https://www.linkedin.com/in/mateusfelix/"
-              target="_blank"
-            >
-              <LinkedinIcon className={classes.linkedinIcon} />
-            </Link>
-          </Box>
+    <Box p={5} maxWidth={350} textAlign="right" className={classes.root}>
+      <Box
+        width={1}
+        display="flex"
+        alignItems="center"
+        justifyContent="flex-end"
+      >
+        <Box mr={1}>
+          <Link href="https://github.com/thebinaryfelix" target="_blank">
+            <GithubIcon className={classes.githubIcon} />
+          </Link>
         </Box>
-        <Box my={1}>
-          <Typography variant="h4" color="secondary">
-            Web Developer
-          </Typography>
+        <Box>
+          <Link href="https://www.linkedin.com/in/mateusfelix/" target="_blank">
+            <LinkedinIcon className={classes.linkedinIcon} />
+          </Link>
         </Box>
-        <Typography variant="h5" component="p" color="secondary">
-          I build web applications and contribute to open-source projects
+      </Box>
+      <Box my={1}>
+        <Typography variant="h4" color="secondary">
+          Web Developer
         </Typography>
-      </Grid>
-    </Grid>
+      </Box>
+      <Typography variant="h5" component="p" color="secondary">
+        I build web applications and contribute to open-source projects
+      </Typography>
+    </Box>
   )
 }
 
--- src/components/ProfileDescription/ProfileDescription.styles.js
+++ src/components/ProfileDescription/ProfileDescription.styles.js
@@ -1,6 +1,5 @@
 import { makeStyles } from '@material-ui/styles'
 import backgroundXs from './images/backgroundXs.svg'
-import backgroundSm from './images/backgroundSm.svg'
 
 export default makeStyles(
   theme => {
@@ -8,17 +7,10 @@ export default makeStyles(
 
     return {
       root: {
-        maxWidth: 330,
-        margin: theme.spacing(2),
-        padding: theme.spacing(3),
-        textAlign: 'right',
         backgroundSize: 'contain',
         backgroundPosition: 'right',
         backgroundRepeat: 'no-repeat',
         backgroundImage: `url(${backgroundXs})`,
-        [theme.breakpoints.up('sm')]: {
-          backgroundImage: `url(${backgroundSm})`,
-        },
       },
       githubIcon: {
         fontSize: theme.typography.pxToRem(40),
