27ca2f23d2b8b47d41280a1783f974ebd105fa7c
thebinaryfelix <mateus.cfoliveira@gmail.com>
2020-06-25 12:24:27 -0300

    refactor(gatsby-config): setup relative paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,34 @@
+const path = require('path')
+
 module.exports = {
   siteMetadata: {
-    title: 'Mateus Felix Web Developer',
-    description: 'Profile page for Mateus Felix',
-    author: '@thebinaryfelix - Mateus Felix',
+    title: `Mateus Felix Web Developer`,
+    description: `Profile page for Mateus Felix`,
+    author: `@thebinaryfelix - Mateus Felix`,
+    siteUrl: `https://thebinaryfelix.me`,
   },
+  pathPrefix: '/',
   plugins: [
-    'gatsby-theme-material-ui',
     {
-      resolve: 'gatsby-plugin-web-font-loader',
+      resolve: `gatsby-plugin-sitemap`,
+    },
+    {
+      resolve: `gatsby-theme-material-ui`,
+      options: {
+        webFontsConfig: {
+          fonts: {
+            google: [
+              {
+                family: `Comfortaa`,
+                variants: [`300`, `400`, `500`],
+              },
+            ],
+          },
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-web-font-loader`,
       options: {
         google: {
           families: ['Comfortaa'],
@@ -15,15 +36,24 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: 'Mateus Felix',
-        short_name: 'Mateus Felix',
-        start_url: '/',
-        background_color: '#FFECAE',
-        theme_color: '#8800FF',
-        display: 'standalone',
-        icon: 'src/images/favicon.png',
+        name: `images`,
+        path: path.join(__dirname, 'src', 'assets', 'images'),
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mateus Felix`,
+        short_name: `Mateus Felix`,
+        start_url: `/`,
+        background_color: `#FFECAE`,
+        theme_color: `#8800FF`,
+        display: `standalone`,
+        icon: `src/assets/images/favicon.png`,
       },
     },
   ],
